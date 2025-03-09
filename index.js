8156334f70756348f963917bb5b7b47a1226e971
Gerardine03 <gerardinefolly@gmail.com>
2025-03-09 23:38:25 +0100

    projet finalisé

---
+++ index.js
@@ -0,0 +1,13 @@
+let body = document.querySelector(".body");
+let rgb = document.querySelector(".rgb");
+
+function changecouleur() {
+    let r = Math.floor(Math.random() *256);
+    let g = Math.floor(Math.random() *256);
+    let b = Math.floor(Math.random() *256);
+    let couleur = `rgb(${r}, ${g}, ${b})`;
+    body.style.backgroundColor = couleur;
+    rgb.textContent =couleur;
+
+}
+setInterval(changecouleur, 1000)
